9be0d5c63b71e70d3d912302d8a713e61ce5f2f7
Polaris-P <pengguo2022@163.com>
2022-05-10 18:41:06 +0800

    提交消息

---
+++ learn/vuecli-demo/src/App.vue
@@ -0,0 +1,29 @@
+<template>
+  <div id="app">
+    
+    <TestVue msg="Welcome to Your Vue.js App"/>
+  </div>
+</template>
+
+<script>
+import TestVue from "./components/TestVue.vue"
+
+export default {
+  name: 'App',
+  components: {
+    // eslint-disable-next-line no-undef
+    TestVue,
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
---
+++ learn/vuecli-demo/vue.config.js
@@ -0,0 +1,8 @@
+module.exports = {
+  devServer: { // 自定义服务配置
+    open: true, // 自动打开浏览器
+    port: 3000,
+    lintOnSave: false // 关闭eslint检查
+  }
+  
+}
